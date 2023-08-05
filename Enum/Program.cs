b8807e4a3a79e7318f83db18047353c856b59fbb
AlpDereli <alpdereli26@gmail.com>
2023-08-05 19:36:12 +0300

    Add project files.

---
+++ Enum/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.SymbolStore;
+
+Console.WriteLine(Gunler.Cumartesi);
+Console.WriteLine((int)Gunler.Cumartesi);
+
+
+
+
+int s = 44;
+if (s < (int)sicaklik.normal)
+{
+    Console.WriteLine("Disari cikmak icin havanin isinmasini bekle");
+}
+if (s>=(int)sicaklik.coksicak)
+{
+    Console.WriteLine("Sakin disari cikma");
+}
+
+
+enum Gunler
+{
+    Pazartesi,
+    Sali,
+    Carsamba,
+    Persembe,
+    Cuma = 23,
+    Cumartesi,
+    Pazar
+}
+
+enum sicaklik
+{
+    soguk = 5,
+    normal =20,
+    sicak = 30,
+    coksicak=40
+}
